bfa2e487e0d247c60248386d9ea1b02cc709c1af
Wu Junkai <wu.junkai@qq.com>
2026-03-29 21:45:19 +0800

    feat: export Distiller, Entity, Relation, KnowledgeGraph

--- src/disk_kg/__init__.py
+++ src/disk_kg/__init__.py
@@ -1,4 +1,6 @@
 from .core import DISK
+from .distiller import Distiller
+from .models import Entity, KnowledgeGraph, Relation
 
-__all__ = ["DISK"]
+__all__ = ["DISK", "Distiller", "Entity", "Relation", "KnowledgeGraph"]
 __version__ = "1.1.0"
